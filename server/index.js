cdecd2bff8b2c15c1b25340600c44433162c0dd3
dominiq1 <dominiqmartinez13@gmail.com>
2023-03-12 20:00:12 -0700

    change

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
   //here is the magic
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'..',   'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname,'..',   'client', 'build', 'index.html'));s
   });
   
 
