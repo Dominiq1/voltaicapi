b25ccaa1b281c6708c11f0102ddd8c40044a33a8
dominiq1 <dominiqmartinez13@gmail.com>
2023-03-13 09:31:40 -0700

    fixed

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,26 @@ const mongoose = require('mongoose');
 
 
 
+// const connectDB = async () => {
+
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+
+// };
+
 
 const connectDB = async () => {
-
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
-
-};
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI);
+      console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+    } catch (err) {
+      console.error(`Error connecting to MongoDB: ${err.message}`.red.bold);
+      process.exit(1); // exit process with failure
+    }
+  };
+  
+  module.exports = connectDB;
+  
 
 module.exports = connectDB;
 
