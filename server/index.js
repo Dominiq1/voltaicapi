da53a73736d8c4032099d49fc053f95046206f45
dominiq1 <dominiqmartinez13@gmail.com>
2023-03-17 10:17:52 -0700

    heroku

--- server/index.js
+++ server/index.js
@@ -2,17 +2,15 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const colors = require('colors');
+require('dotenv').config();
 const schema = require('./schema/schema')
 const connectDB = require('./config/db');
 const cors = require('cors');
-const path = require('path');
-//new
 
 
 const port = process.env.PORT || 5000;
-
+// process.env.PORT || 
 const app = express();
-app.use(express.static('public'));
 
 
 //Connect to database
@@ -21,27 +19,59 @@ app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV !== 'production'
+    graphiql : process.env.NODE_ENV ==='development'
 }))
 
-
-// app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
-  
-
-
-  //here is the magic
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'public','index.html'));
-  });
-  
-
-
-
-
 app.listen(port, console.log(`Server running on port ${port}`));
+
+
+
+
+// const express = require('express');
+// const { graphqlHTTP } = require('express-graphql');
+// const colors = require('colors');
+// const schema = require('./schema/schema')
+
+// require('dotenv').config();
+// const connectDB = require('./config/db');
+// const cors = require('cors');
+// const path = require('path');
+// //new
+
+
+// const port = process.env.PORT || 5000;
+
+// const app = express();
+// app.use(express.static('public'));
+
+
+// //Connect to database
+// connectDB();
+// app.use(cors());
+
+// app.use('/graphql', graphqlHTTP({
+//     schema,
+//     graphiql: process.env.NODE_ENV !== 'production'
+// }))
+
+
+// // app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).send('Something broke!');
+//   });
+  
+
+
+//   //here is the magic
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '..', 'public','index.html'));
+//   });
+  
+
+
+
+
+// app.listen(port, console.log(`Server running on port ${port}`));
