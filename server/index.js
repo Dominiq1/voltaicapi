ad1adafa14f2d86fe9eec8d9c079c2b251d1c930
dominiq1 <dominiqmartinez13@gmail.com>
2023-03-20 15:20:42 -0700

    server

--- server/index.js
+++ server/index.js
@@ -7,8 +7,12 @@ require('dotenv').config();
 const connectDB = require('./config/db');
 const cors = require('cors');
 const path = require('path');
+const next = require('next');
 
 const port = process.env.PORT || 5000;
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
 
 const app = express();
 
@@ -21,7 +25,7 @@ app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV !== 'production'
+    graphiql: dev
 }));
 
 app.use((err, req, res, next) => {
@@ -31,21 +35,12 @@ app.use((err, req, res, next) => {
 
 // Serve the built index.html file
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', '.next'));
+    return handle(req, res);
 });
 
-app.listen(port, console.log(`Server running on port ${port}`));
-
-
-
-
-
-
-
-
-
-
-
+nextApp.prepare().then(() => {
+  app.listen(port, console.log(`Server running on port ${port}`));
+});
 
 
 
